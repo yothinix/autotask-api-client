63a14cab0bd8d711c9a055d1f5a034c2cd009f33
yothinix <m@yothinix.com>
2018-02-21 11:55:07 +0700

    Read xml template from file

--- main.py
+++ main.py
@@ -16,30 +16,28 @@ def get_xml_field_value(field_name, data):
 
 def main():
     with open('query_ticket.xml', 'r') as xml_file:
-        xml = xml_file.read()
+        xml_template = xml_file.read()
 
+    url = 'https://webservices2.autotask.net/atservices/1.5/atws.asmx'
+    data = xml_template.format(
+        filter_field='ticketnumber',
+        ticket_number='T20180220.0001'
+    )
+    headers = {'Content-Type': 'text/xml'}
     auth = HTTPBasicAuth(
         os.environ.get('AUTOTASK_USERNAME'),
         os.environ.get('AUTOTASK_PASSWORD')
     )
 
-    res = requests.post(
-       'https://webservices2.autotask.net/atservices/1.5/atws.asmx',
-        data=xml.format(filter_field='ticketnumber', ticket_number='T20180220.0001'),
-        headers={'Content-Type': 'text/xml'},
-        auth=auth
-    )
-
-    data =xmltodict.parse(res.text)
+    res = requests.post(url, data, headers=headers, auth=auth)
+    data = xmltodict.parse(res.text)
 
     ticket = {}
     for key in ('AccountID', 'CreateDate', 'DueDateTime', 'TicketNumber', 'Title', 'Description', 'AssignedResourceID'):
         ticket[key] = get_xml_field_value(key, data)
 
-    import ipdb
-    ipdb.set_trace()
 
-    print('hello world')
-
+    print(ticket)
+    return ticket
 
 main()
