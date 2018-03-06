9be5975e69d493dbc096a201541c5eaf28c32238
yothinix <m@yothinix.com>
2018-03-06 17:52:15 +0700

    Update to latest version with type annotation
    - Also add flake8 config file

--- autotask.py
+++ autotask.py
@@ -1,7 +1,22 @@
+import os
+from collections import OrderedDict
+from typing import Tuple, Union, Dict
+
+from django.conf import settings
+
 import requests
 import xmltodict
 from requests.auth import HTTPBasicAuth
+from rest_framework import status
 from toolz import get_in
+
+
+ENTITY_DEFAULT_FIELDS = {
+    'Ticket': ('id', 'AccountID', 'CreateDate', 'CreatorResourceID', 'DueDateTime', 'LastActivityDate',
+               'Priority', 'AssignedResourceID', 'AssignedResourceRoleID', 'Status', 'TicketNumber',
+               'Title', 'Resolution', 'PurchaseOrderNumber', 'TicketType', 'ChangeApprovalType',
+               'LastCustomerVisibleActivityDateTime', 'TicketCategory', 'ExternalID')
+}
 
 
 class Autotask():
@@ -14,80 +29,86 @@ class Autotask():
             '@xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
             '@xmlns:xsd': 'http://www.w3.org/2001/XMLSchema',
             '@xmlns:soap': 'http://schemas.xmlsoap.org/soap/envelope/',
+            'soap:Header': {
+                'IntegrationCode': settings.SIMPLESAT_AUTOTASK_INTEGRATION_CODE
+            },
             'soap:Body': {}
         }
     }
 
+    def _request(self, data: str) -> Tuple[int, str]:
+        auth = HTTPBasicAuth(self.username, self.password)
+        res = requests.post(self.url, data, headers=self.headers, auth=auth)
+        return res.status_code, res.text
+
     @staticmethod
-    def get_xml_field_value(field_name, data, mode='query'):
-        entity_path = [
-            'soap:Envelope',
-            'soap:Body',
-            f'{mode}Response',
-            f'{mode}Result',
-            'EntityResults',
-            'Entity'
-        ]
+    def get_field_value(
+            field_name: str,
+            data: Dict[str, Union[str, int, Dict]],
+            mode: str = 'query'
+    ) -> Union[str, int]:
+
+        entity_path = ['soap:Envelope', 'soap:Body', f'{mode}Response', f'{mode}Result', 'EntityResults', 'Entity']
         if field_name == 'id':
             return get_in(entity_path + [field_name], data)
 
         return get_in(entity_path + [field_name, '#text'], data)
 
-    def _request(self, data):
-        auth = HTTPBasicAuth(self.username, self.password)
-        res = requests.post(self.url, data, headers=self.headers, auth=auth)
+    def extract_response_from_keys(
+            self,
+            response_body: Dict[str, Union[str, int, Dict]],
+            select_fields: Tuple[str, ...],
+            mode: str
+    ) -> Dict[str, Union[str, int]]:
 
-        return xmltodict.parse(res.text)
-
-    def extract_response_from_keys(self, response_body, select_fields, mode):
         entity = {}
         for key in select_fields:
-            entity[key] = self.get_xml_field_value(key, response_body, mode)
+            entity[key] = self.get_field_value(key, response_body, mode)
+
         return entity
 
-    def query(self, entity, filter_field, filter_value, select_fields=()):
-        with open('templates/query.xml', 'r') as xml_file:
+    def query(
+            self,
+            entity: str,
+            filter_field: str,
+            filter_value: str,
+            operation: str = 'equals',
+            select_fields: Tuple[str, ...] = ()
+    ) -> Tuple[Union[Dict, str], int]:
+
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.abspath(os.path.join(current_dir, 'xml/query.xml'))
+        with open(file_path, 'r') as xml_file:
             xml_template = xml_file.read()
 
         data = xml_template.format(
             entity=entity,
+            operation=operation,
             filter_field=filter_field,
             filter_value=filter_value
         )
 
-        response_body = self._request(data)
+        status_code, body = self._request(data)
+
+        if status_code != status.HTTP_200_OK:
+            return body, status_code
 
         entity = self.extract_response_from_keys(
-            response_body, select_fields, mode='query'
+            response_body=xmltodict.parse(body),
+            select_fields=select_fields if select_fields else ENTITY_DEFAULT_FIELDS[entity],
+            mode='query'
         )
-        return entity
 
-    def create(self, entity, update_object, select_fields=()):
-        procedure = {
-            'create': {
-                '@xmlns': 'http://autotask.net/ATWS/v1_5/',
-                'Entities': {
-                    'Entity': {
-                        '@xsi:type': entity,
-                        **update_object
-                    }
-                }
-            }
-        }
+        return entity, status_code
 
-        create_procedure = self.base_xml_template.copy()
-        create_procedure['soap:Envelope']['soap:Body'] = procedure
-
-        data = xmltodict.unparse(create_procedure)
-
-        response_body = self._request(data)
-
-        entity = self.extract_response_from_keys(
-            response_body, select_fields, mode='create'
-        )
-        return entity
-
-    def update_udf(self, entity, lookup_keys, field, value, select_fields=()):
+    def update_udf(
+        self,
+        entity: str,
+        lookup_keys: Dict[str, str],
+        field: str,
+        value: str,
+        select_fields: Tuple[str, ...] = ()
+    ) -> Tuple[Union[Dict, str], int]:
         procedure = {
             'update': {
                 '@xmlns': 'http://autotask.net/ATWS/v1_5/',
@@ -111,14 +132,20 @@ class Autotask():
 
         data = xmltodict.unparse(create_procedure)
 
-        response_body = self._request(data)
+        status_code, body = self._request(data)
+
+        if status_code != status.HTTP_200_OK:
+            return body, status_code
 
         entity = self.extract_response_from_keys(
-            response_body, select_fields, mode='update'
+            response_body=xmltodict.parse(body),
+            select_fields=select_fields if select_fields else ENTITY_DEFAULT_FIELDS,
+            mode='update'
         )
+
         return entity
 
-    def get_zone_info(self):
+    def get_zone_info(self) -> Tuple[Union[OrderedDict, str], int]:
         procedure = {
             'getZoneInfo': {
                 '@xmlns': 'http://autotask.net/ATWS/v1_5/',
@@ -131,7 +158,10 @@ class Autotask():
 
         data = xmltodict.unparse(base_procedure)
 
-        response_body = self._request(data)
+        status_code, body = self._request(data)
+
+        if status_code != status.HTTP_200_OK:
+            return body, status_code
 
         keys = [
             'soap:Envelope',
@@ -139,5 +169,9 @@ class Autotask():
             'getZoneInfoResponse',
             'getZoneInfoResult'
         ]
+        response_body = xmltodict.parse(body)
         entity = get_in(keys, response_body)
-        return entity
+
+        self.url = entity['URL']
+
+        return entity, status_code
